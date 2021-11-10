788b7a80ed469b72bbc1ef7d3b38c4bf992f03ad
jj <jotajota1302@hotmail.com>
2021-11-10 18:47:50 +0100

    commit

---
+++ databaseExamples/src/main/java/edu/es/eoi/entity/Cliente.java
@@ -0,0 +1,36 @@
+package edu.es.eoi.entity;
+
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "clientes")
+public class Cliente {
+	
+	@Id
+	@Column(name = "DNI", unique = true)
+	private String dni;
+
+	@Column(name = "NOMBRE")
+	private String nombre;	
+
+	@Column(name = "alta")
+	private Date fechaAlta;
+	
+	@Column(name = "DIRECCION")
+	private String direccion;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "cliente")
+	private List<Cuenta> cuentas;
+	
+}
