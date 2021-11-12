095efb2b7cf892d1566b2daf6b49d22730b2d63b
jj <jotajota1302@hotmail.com>
2021-11-12 15:08:32 +0100

    commit

--- twitch/src/main/java/edu/es/eoi/Main.java
+++ twitch/src/main/java/edu/es/eoi/Main.java
@@ -1,5 +1,8 @@
 package edu.es.eoi;
 
+import java.util.ArrayList;
+
+import edu.es.eoi.entity.Direccion;
 import edu.es.eoi.entity.Persona;
 import edu.es.eoi.entity.Subscripcion;
 import edu.es.eoi.repository.CategoriaRepositoryJPAImpl;
@@ -37,6 +40,26 @@ public class Main {
 		p.setSubscripcion(s);
 		s.setPersona(p);
 
+		//relacion 1-N
+		
+		Direccion direccion1= new Direccion();
+		direccion1.setCalle("C/ Direccion Uno");
+		direccion1.setCodigopostal("03550");
+		
+		Direccion direccion2= new Direccion();
+		direccion2.setCalle("C/ Direccion Dos");
+		direccion1.setCodigopostal("03350");
+		
+		direccion1.setPersona(p);
+		direccion2.setPersona(p);
+		
+		p.setDirecciones(new ArrayList<Direccion>());
+		
+		p.getDirecciones().add(direccion1);
+		p.getDirecciones().add(direccion2);
+		
+		//N:N igual pero con 2 set en cada lado	
+		
 		personaRepositoryJPAImpl.save(p);
 		
 		personaRepositoryJPAImpl.remove(p.getId());
