cf921a59a5cddb32676aaf34942dfbb29cedffef
jj <jotajota1302@hotmail.com>
2021-11-16 15:20:02 +0100

    commit

---
+++ twitch-springboot/src/main/java/edu/es/eoi/twitchspringboot/TwitchSpringbootApplication.java
@@ -0,0 +1,13 @@
+package edu.es.eoi.twitchspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitchSpringbootApplication.class, args);
+	}
+
+}
---
+++ twitch-springboot/src/test/java/edu/es/eoi/twitchspringboot/TwitchSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.es.eoi.twitchspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitchSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
