23d65bc9ce1d18dc9e424c8feff39a9e68f5ee45
jj <jotajota1302@hotmail.com>
2021-11-16 18:23:13 +0100

    commit

--- twitch-springboot-data/src/main/java/edu/es/eoi/TwitchSpringbootDataApplication.java
+++ twitch-springboot-data/src/main/java/edu/es/eoi/TwitchSpringbootDataApplication.java
@@ -2,10 +2,12 @@ package edu.es.eoi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@ComponentScan(basePackages = "edu.es.eoi")
 public class TwitchSpringbootDataApplication {
 
 	public static void main(String[] args) {
